bbcc5125660945f567bc4fd62fdd8fbdcf7088f9
nickborbe <nickborbe@gmail.com>
2018-06-15 15:25:52 -0400

    add DOM intro

---
+++ day5-dom-intro/app.js
@@ -0,0 +1,33 @@
+
+var theButton = document.getElementById('the-button');
+var theList = document.getElementsByClassName('theList')[0];
+
+
+theButton.onclick = function(){
+
+    var howManyTimes = document.getElementById('quantity');
+    var quantity = howManyTimes.value;
+    
+    if(quantity ===""){quantity = 1};
+
+    for(var i = 0; i < quantity; i++){
+
+    var theInput = document.getElementById('the-input');
+
+    var whatToAppend = theInput.value;
+
+    var newNew = document.createElement('li');
+
+    newNew.innerHTML = whatToAppend;
+
+    theList.appendChild(newNew);
+    }
+
+    theInput.value = "";
+    howManyTimes.value = "";
+
+
+  }
+
+
+
