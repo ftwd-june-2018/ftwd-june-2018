54b414400be6685e525533e7dc1fe64526f2c627
nickborbe <nickborbe@gmail.com>
2018-07-02 16:27:28 -0400

    intro to express addeadded

---
+++ week3/intro-to-server-js/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+//localhost:3000/images/cat.jpg
+//will serve the cat picture and we dont need to include the word
+//public in the url
+
+// our first Route
+app.get('/', (req, res, next) =>  {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+
+app.get('/about-us', (req, res, next) =>  {
+    res.sendFile(__dirname + '/views/about-us.html');
+});
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+  });
---
+++ week3/npm-example/app.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+
+
+
+console.log(chalk.red('Hello, npm!'));
+console.log(chalk.cyan('Hello, npm!'));
+console.log(chalk.magenta('Hello, npm!'));
+console.log(chalk.green('Hello, npm!'));
+console.log(chalk.yellow('Hello, npm!'));
