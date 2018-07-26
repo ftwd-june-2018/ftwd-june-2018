d3efafc40fd3d5b9a3afb693213b3546831469da
nickborbe <nickborbe@gmail.com>
2018-07-26 09:37:14 -0400

    module

--- week7/iron-nutrition/starter-code/src/app/app.module.ts
+++ week7/iron-nutrition/starter-code/src/app/app.module.ts
@@ -6,19 +6,23 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { StringifyPipe } from './stringify.pipe';
+import { FoodService } from './services/food.service';
+import { AddFoodComponent } from './add-food/add-food.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
     StringifyPipe,
+    AddFoodComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [FoodService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
