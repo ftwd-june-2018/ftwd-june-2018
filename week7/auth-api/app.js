c67ad70c8d46e669e419934f027dbeb449da3a9c
nickborbe <nickborbe@gmail.com>
2018-07-30 12:52:51 -0400

    angular app consumes express api for authentication

--- week7/auth-api/app.js
+++ week7/auth-api/app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const cors         = require('cors');
+
 const session    = require('express-session');
 
 const passport     = require('passport');
@@ -68,7 +70,7 @@ app.use(passport.session());
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+app.use(cors());
 
 const index = require('./routes/index');
 app.use('/', index);
